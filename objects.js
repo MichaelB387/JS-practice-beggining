1bf07ec13dceaa72878371c1db32023b90dfdca7
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-26 13:04:43 +1100

    objects example

---
+++ objects.js
@@ -0,0 +1,13 @@
+
+// This is an object
+var myComputer = {
+    // operatingSystem is the property, 'mac' is the value
+    operatingSystem: 'mac',
+    // screenSize is the property, '11 inches' is the value    
+    screenSize: '11 inches',
+    // purchaseYear is the property, 2014 is the value which is an integer   
+    purchaseYear: 2014
+}
+
+// This will output the operating system value mac to the consoles
+console.log(myComputer.operatingSystem)

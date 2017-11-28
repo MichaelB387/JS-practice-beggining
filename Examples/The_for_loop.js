c2c3d2026e166844be29a2387df62af0a51f1eeb
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-28 15:29:37 +1100

    corrected syntax

--- Examples/The_for_loop.js
+++ Examples/The_for_loop.js
@@ -1,6 +1,6 @@
 // i = 0                Initialization
 // Say "hey" if i < 3   Condition
-// Increase i by 1      fianl-expression
+// Increase i by 1      final-expression
 
 // 0 "hey"
 // 1 "hey"
